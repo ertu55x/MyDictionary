d48ac5e12bfa560b5b44aedcb2157c3574202835
Ertuğrul Gül <ertugul.55@gmail.com>
2021-07-19 18:23:33 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        private K[] keys;
+        private V[] values;
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+        public void Add(K key, V value)
+        {
+            K[] arrayKey = keys;
+            keys = new K[keys.Length + 1];
+            for (int i = 0; i < arrayKey.Length; i++)
+            {
+                keys[i] = arrayKey[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            V[] arrayValue = values;
+            values = new V[values.Length + 1];
+            for (int i = 0; i < arrayValue.Length; i++)
+            {
+                values[i] = arrayValue[i];
+            }
+            values[values.Length - 1] = value;
+        }
+        public int Lenght
+        {
+            get { return keys.Length; }
+        }
+        public K[] Keys
+        {
+            get { return keys; }
+        }
+        public V[] Values
+        {
+            get { return values; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+
+            students.Add(5534, "Ertuğrul");
+            students.Add(9054, "Ertuğ");
+            students.Add(6041, "Ertan");
+
+            foreach (var student in students.Keys)
+            {
+                Console.WriteLine(student);
+            }
+           
+            Console.ReadLine();
+        }
+    }
+   
+}
